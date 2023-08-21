2e909e2d2a64a09b2d2192b60f83272b053258e8
unnath123 <64188873+unnath123@users.noreply.github.com>
2023-08-21 23:50:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,14 +8,12 @@ const person = {
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
 
-
 let team=players;
-let team1=[];
-	for(let i=0;i<players.length;i++){
-	team1[i]=players[i];
-	}
+let team1=players.slice(0);
 
 
+let cap1={
+	  name: "John Doe",
+  age: 80,
+};
 
-
-let cap1=person;
